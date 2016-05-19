de99ef09899630ddfc0147a7765fdc610216296b
Ma Junbao <mjbwork12@gmail.com>
2016-05-19 17:18:36 +0800

    更新query

--- module/query/query.js
+++ module/query/query.js
@@ -1,30 +1,15 @@
 var $$ = function(selector, children) {
-  return _instance();
-  // if(!selector){
-  //   return _instance();
-  // }else if(selector){
-  //   // fun
-  // }else{
-  //   // dom
-  //   var dom = '<h1>This is h1.</h1>'
-  //   return _instance(dom, selector);
-  // }
+  this.selector = selector;
+  return this;
 }
-
-var _instance = function(dom, selector){
-  var dom = dom || [];
-  console.log(1)
-  dom.selector = selector;
-  if (!(this instanceof _instance)) {
-    console.log(2)
-    return new _instance;
-    
-  }
-  
+$$.fn = $$.prototype = {
+  selectorName: function() {
+    return this.selector;
+  },
+  constructor: $$
 }
+var a = new $$('aaa');
 
-_instance.prototype = $$.fn = {};
-
-$$.fn.a = 2;
+console.log(a.selectorName())
 
 export default $$;
