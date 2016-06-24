870bcf1011b0d0d0524d45b86f0193e954aff8a9
Ma Junbao <mjbwork12@gmail.com>
2016-06-24 17:11:27 +0800

    build升级

--- build.js
+++ build.js
@@ -1,90 +1,215 @@
-"use strict";
-
+const yaml = require('js-yaml');
 const fs = require('fs');
-const http = require('http');
+const path = require('path');
 const sass = require('node-sass');
+const colors = require('colors');
+const package = require('./package.json')
+var cleancss = require('clean-css');
 
-// css文件
+let config;
 
-const source = {
-  css: 'module/init.css',
-  js: 'module/init.js'
+try {
+  config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+} catch (e) {
+  console.log(e);
 }
-const dist = {
-  css: ['dist/ui.css','dajisu/css/ui.css'],
-  js: 'dist/ui.js'
-}
 
+fs.watchFile('config.yml', (curr, prev) => {
+  config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+})
 
+let theme = config.theme;
+let root = config.path;
+let modules = config.modules;
+let outs = config.out;
 
-function buildCss(filename) {
-  // 处理css程序
-  sass.render({
-    file: 'module/init.scss',
-    outputStyle: 'expanded'
-  },function(error, result){
-    if (error) {
-      console.log(error.formatted);
-    }else{
-      if(typeof(dist.css) === 'string'){
-        fs.writeFile(dist.css, result.css.toString(),function(){
-          console.log(new Date().toLocaleTimeString() + ' ' + filename + ' 保存成功。ui.css 编译成功。');  
+let styleExtname = '.scss';
+let styleSource;
+let styleCompile;
+let styleMin;
+
+let scriptExtname = '.js';
+let scriptSource;
+let scriptCompile;
+let scriptMin;
+
+// 获取 js 或 css 路径
+let paths = (filter, pathsArray = outs) => {
+  if (Array.isArray(pathsArray)) {
+    let _paths;
+    switch (filter) {
+      case '.min.css':
+      case '.min.js':
+        _paths = outs.filter(function(val) {
+          return val.substr(-filter.length, filter.length) === filter
         })
-      }else if(Array.isArray(dist.css)){
-        var resultCss = result.css.toString();
-        dist.css.forEach(function(val){
-          fs.writeFile(val, resultCss,function(){
-            console.log(new Date().toLocaleTimeString() + ' ' + filename + ' 保存成功。'+ val + ' 编译成功。');  
-          })
-        })
-      }
-      
+        break;
+      case '.css':
+      case '.js':
+        _paths = outs.filter(function(val) {
+          return val.substr(-filter.length, filter.length) === filter
+        });
+        _paths = _paths.filter(function(val) {
+          return val.substr(-`.min${filter}`.length, `.min${filter}`.length) !== `.min${filter}`
+        });
+        break;
     }
-  })
+    return _paths;
+
+  }
 }
 
-function buildJs(filename) {
-  // 处理js程序
-  rollup.rollup({
-    entry: source.js,
-    plugins: [
-      includePaths({
+let css = (event = '', filename = '') => {
 
-      }),
-      eslint({
-        "parserOptions": {
-          "sourceType": "module"
+  styleSource = (() => {
+    let _styleSource;
+    // 载入主题
+    if (theme) {
+      try {
+        _styleSource = fs.readFileSync(path.join(root, 'theme', theme + styleExtname), 'utf8');
+      } catch (err) {
+        console.log(Error(err))
+      }
+    } else {
+      console.error('失败：CSS 主题没有设置');
+      return;
+    }
+    // 处理模块
+    if (Array.isArray(modules)) {
+      for (let module of modules) {
+        let modulePath = path.join(root, module, module + styleExtname);
+        try {
+          _styleSource += '\n' + fs.readFileSync(modulePath, 'utf8');
+        } catch (err) {
+          console.log(Error(err))
         }
-      })
-    ]
-  }).then(function(bundle) {
-    bundle.write({
-      format: 'iife',
-      moduleName: 'ui',
-      dest: dist.js
-    });
-    console.log(new Date().toLocaleTimeString() + ' ' + filename + ' 保存成功。ui.js 编译成功。');
-  })
-}
+      }
+    } else {
+      console.error('失败：CSS 模块没有设置');
+      return;
+    }
+    return _styleSource;
+  })();
 
-function buildHtml() {
+  styleCompile = (() => {
+    // 编译scss
+    let _styleCompile;
+    try {
+      _styleCompile = sass.renderSync({
+        data: styleSource,
+        outputStyle: 'expanded',
+        includePaths: modules.map(function(val) {
+          return path.join(root, val)
+        })
+      }).css.toString();
+    } catch (err) {
+      console.log(Error(err))
+    }
+    return _styleCompile;
+  })();
 
-}
+  styleMin = (() => {
+    let _styleMin;
+    try {
+      _styleMin = new cleancss({
+        compatibility: 'ie7',
+        root: './',
+        rebase: false
+      }).minify(styleCompile).styles;
+    } catch (err) {
+      console.log(Error(err))
+    }
+    return _styleMin;
+  })();
 
-function watch() {
-  fs.readdir('module', (err, files) => {
-    files.forEach(function(file) {
-      fs.watch('module/' + file, function(err, filename) {
-        if (filename) {
-          if (filename.indexOf('.scss') > -1) {
-            buildCss(filename);
-          } else if (filename.indexOf('.js') > -1) {
-            buildJs(filename);
-          }
+  (() => {
+    // 写入文件
+    let _styleCompilePaths = paths('.css');
+    let _styleMinPaths = paths('.min.css');
+
+    if (Array.isArray(_styleCompilePaths)) {
+      for (let _styleCompilePath of _styleCompilePaths) {
+        try {
+          fs.writeFileSync(_styleCompilePath, styleCompile, 'utf8');
+        } catch (err) {
+          console.log(Error(err))
         }
-      })
-    })
-  })
+      }
+    } else {
+      console.error('失败：CSS 输出目录没有设置，请配置 out');
+      return;
+    }
+
+    if (Array.isArray(_styleMinPaths)) {
+      for (let _styleMinPath of _styleMinPaths) {
+        try {
+          fs.writeFileSync(_styleMinPath, styleMin, 'utf8');
+        } catch (err) {
+          console.log(Error(err))
+        }
+      }
+    } else {
+      console.error('失败：CSS 输出目录没有设置，请配置 out');
+      return;
+    }
+  })();
+
+  (() => {
+    // 打印日志
+    if (event && filename) {
+      console.log(" ✓".green, `${filename} `.grey, event.grey, new Date().toLocaleTimeString().grey)
+    }
+  })()
+
 }
 
+let watch = () => {
+  for (let module of modules) {
+    try {
+      fs.watch(path.join(root, module), function(event, filename) {
+        css(event, filename);
+      })
+    }catch(err){
+      console.log(Error(err))
+    }
+
+  }
+}
+
+let log = () => {
+  process.stdout.write("\u001B[2J\u001B[0;0f")
+  console.log("========================================================================")
+  console.log(` UICSS v${package.version}`, "- 简单 (但是强壮) 的前端 UiKit".grey)
+  console.log(" https://github.com/majunbao/uicss".grey)
+  console.log("========================================================================")
+  console.log(" ");
+  console.log(` ▣ MODULES`.toUpperCase())
+  for (let module of modules) {
+    console.log(" ✓".green, module.replace(module.charAt(0), module.charAt(0).toUpperCase()).grey)
+  };
+  console.log(' ');
+  console.log(` ▣ OUTS`)
+  for (let out of outs) {
+    console.log(" ✓".green, out.grey)
+  };
+  console.log(' ')
+}
+
+
+log()
+css()
 watch()
+
+
+// 配置变化时 重新配置上下文
+fs.watch('config.yml', (evnet, filename) => {
+  config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+
+  theme = config.theme;
+  root = config.path;
+  modules = config.modules;
+  outs = config.out;
+
+  log()
+  css()
+})
---
+++ old_build.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const fs = require('fs');
+const http = require('http');
+const sass = require('node-sass');
+
+// css文件
+
+const source = {
+  css: 'module/init.css',
+  js: 'module/init.js'
+}
+const dist = {
+  css: ['dist/ui.css','dajisu/css/ui.css'],
+  js: 'dist/ui.js'
+}
+
+
+
+function buildCss(filename) {
+  // 处理css程序
+  sass.render({
+    file: 'module/init.scss',
+    outputStyle: 'expanded'
+  },function(error, result){
+    if (error) {
+      console.log(error.formatted);
+    }else{
+      if(typeof(dist.css) === 'string'){
+        fs.writeFile(dist.css, result.css.toString(),function(){
+          console.log(new Date().toLocaleTimeString() + ' ' + filename + ' 保存成功。ui.css 编译成功。');  
+        })
+      }else if(Array.isArray(dist.css)){
+        var resultCss = result.css.toString();
+        dist.css.forEach(function(val){
+          fs.writeFile(val, resultCss,function(){
+            console.log(new Date().toLocaleTimeString() + ' ' + filename + ' 保存成功。'+ val + ' 编译成功。');  
+          })
+        })
+      }
+      
+    }
+  })
+}
+
+function buildJs(filename) {
+  // 处理js程序
+  rollup.rollup({
+    entry: source.js,
+    plugins: [
+      includePaths({
+
+      }),
+      eslint({
+        "parserOptions": {
+          "sourceType": "module"
+        }
+      })
+    ]
+  }).then(function(bundle) {
+    bundle.write({
+      format: 'iife',
+      moduleName: 'ui',
+      dest: dist.js
+    });
+    console.log(new Date().toLocaleTimeString() + ' ' + filename + ' 保存成功。ui.js 编译成功。');
+  })
+}
+
+function buildHtml() {
+
+}
+
+function watch() {
+  fs.readdir('module', (err, files) => {
+    files.forEach(function(file) {
+      fs.watch('module/' + file, function(err, filename) {
+        if (filename) {
+          if (filename.indexOf('.scss') > -1) {
+            buildCss(filename);
+          } else if (filename.indexOf('.js') > -1) {
+            buildJs(filename);
+          }
+        }
+      })
+    })
+  })
+}
+
+watch()
